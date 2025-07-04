ce8c065a920ca4ae06e0b6e1be91668d5f5ff522
Adebayo114 <matthewadebayo392@gmail.com>
2025-07-04 10:07:51 +0100

    fix up2

--- src/Components/WelcomePage.jsx
+++ src/Components/WelcomePage.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 function WelcomePage() {
   return (
     <div className='container'>
-        <h1 className="bouncing-text">Welcome to Dyna-Quiz</h1>
+        <h1 className="bouncing-text">Welcome to Dyna-TecQ-Quiz</h1>
         <p>Test your knowledge and skills with our dynamic quiz platform.</p>   
         <p>Get ready to challenge yourself and learn something new!</p>
         <p>Click the button below to start your quiz.</p>
